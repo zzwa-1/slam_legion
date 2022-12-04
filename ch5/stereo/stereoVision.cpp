9961a5afb286db38cb2b42ddb4431b5a97fa5564
zzzwa66 <jhan04573@gmail.com>
2022-12-04 15:57:17 +0800

    update

--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -19,33 +19,39 @@ void showPointCloud(
 int main(int argc, char **argv) {
 
     // 内参
-    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
+    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157; //fx和fy的单位是像素
     // 基线
     double b = 0.573;
 
     // 读取图像
-    cv::Mat left = cv::imread(left_file, 0);
+    cv::Mat left = cv::imread(left_file, 0);//0表示返回灰度图像
     cv::Mat right = cv::imread(right_file, 0);
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
-        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数，参数是固定的吗？这是网络上的关于sgbm算法的经典参数配置
     cv::Mat disparity_sgbm, disparity;
     sgbm->compute(left, right, disparity_sgbm);
-    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f); //disparity才是最后的视差图
+    /*
+    用于计算距离的视差图（CV_32F）和用于肉眼看的视差图（CV_8U）使用的格式不同，
+    并且用于计算的视差图无需进行裁剪和归一化，这些只是为了显示的可读性和美观。
+    所以，在对sgbm进行compute之后得到视差图disparity_sgbm，除以16得到用于计算的视差图disparity
+    除以16是因为每个像素值由一个16bit表示，其中低位的4位存储的是视差值得小数部分，所以真实视差值应该是该值除以16
+    */
 
     // 生成点云
-    vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
+    vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;  //定义四维向量
 
     // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
     for (int v = 0; v < left.rows; v++)
-        for (int u = 0; u < left.cols; u++) {
-            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+        for (int u = 0; u < left.cols; u++) {//这里要定义为int类型
+            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue; //一定要注意这里是（v,u),而且视差大于96时舍去
 
             Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
 
             // 根据双目模型计算 point 的位置
             double x = (u - cx) / fx;
             double y = (v - cy) / fy;
-            double depth = fx * b / (disparity.at<float>(v, u));
+            double depth = fx * b / (disparity.at<float>(v, u));  //这里应该是f而不是fx吧
             point[0] = x * depth;
             point[1] = y * depth;
             point[2] = depth;
@@ -53,13 +59,14 @@ int main(int argc, char **argv) {
             pointcloud.push_back(point);
         }
 
-    cv::imshow("disparity", disparity / 96.0);
+    cv::imshow("disparity", disparity / 96.0); //对视差图归一化
     cv::waitKey(0);
     // 画出点云
     showPointCloud(pointcloud);
     return 0;
 }
 
+//固定画图函数
 void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud) {
 
     if (pointcloud.empty()) {
