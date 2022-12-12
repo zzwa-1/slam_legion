509d5793b0bdcafa8715586d81bf408d4f0f0e52
zzzwa66 <jhan04573@gmail.com>
2022-12-12 15:45:57 +0800

    update

---
+++ ch3/visualizeGeometry/README.md
@@ -0,0 +1,1 @@
+#LD_LIBRARY_PATH=/home/zzwa/soft_ware/Pangolin/build ./visualizeGeometry 这句话用得到
--- ch3/visualizeGeometry/build/CMakeFiles/visualizeGeometry.dir/DependInfo.cmake
+++ ch3/visualizeGeometry/build/CMakeFiles/visualizeGeometry.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/zzwa/sjtu/slam_zzwa/ch3/visualizeGeometry/visualizeGeometry.cpp" "/home/zzwa/sjtu/slam_zzwa/ch3/visualizeGeometry/build/CMakeFiles/visualizeGeometry.dir/visualizeGeometry.cpp.o"
+  "/home/zzwa/sjtu/slam_legion/ch3/visualizeGeometry/visualizeGeometry.cpp" "/home/zzwa/sjtu/slam_legion/ch3/visualizeGeometry/build/CMakeFiles/visualizeGeometry.dir/visualizeGeometry.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
